8f873a7bc7536c046e986ad37ccca1fd4c797325
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-05-22 17:32:50 -0300

    ✨feat: renderiza componente checkbox no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./styles/PaginaDeLogin.css";
 import "./styles/styles.css";
 import Title from "./components/Title";
@@ -5,23 +6,40 @@ import SubTitle from "./components/SubTitle";
 import PaginaDeLogin from "./components/PaginaDeLogin";
 import CampoDeDigitacao from "./components/CampoDeDigitacao";
 import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
 
 function App() {
+  const [email, setEmail] = React.useState('');
+  const [senha, setSenha] = React.useState('');
+
+
+
+
   return (
-    <>
-      <PaginaDeLogin>
+    <>  
+      <PaginaDeLogin email={ email} senha={senha}>
         <Title />
         <SubTitle />
         <CampoDeDigitacao
           label="Email ou Usuário"
           tipo="email"
           placeholder="Email ou Usuário"
-        />
+          value={email}
+          setValue={setEmail}
+          />
         <CampoDeDigitacao
           label="Senha"
           tipo="password"
           placeholder="Informe sua Senha"
+          value={senha}
+          setValue={setSenha}
         />
+        <fieldset className="form__opcoes">
+          <Checkbox />
+          <p>
+            <a href="#">Esqueci minha senha</a>
+          </p>
+        </fieldset>
         <Button />
       </PaginaDeLogin>
     </>
