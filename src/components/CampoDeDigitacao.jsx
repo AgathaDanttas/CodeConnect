5441b7068fe2e466908cb7803c14aeb2b47aeb80
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-05-22 17:06:53 -0300

    🐛fix(login): corrige o acesso aos estados email e senha no PaginaDeLogin
    O componente PaginaDeLogin agora recebe email e senha como props, permitindo acesso aos estados gerenciados no App.

--- src/components/CampoDeDigitacao.jsx
+++ src/components/CampoDeDigitacao.jsx
@@ -1,12 +1,19 @@
 import "../styles/CampoDeDigitacao.css";
 import PropTypes from "prop-types";
 
-function CampoDeDigitacao({ label, tipo, placeholder }) {
+function CampoDeDigitacao({ label, tipo, placeholder = '',value = '',setValue }) {
   return (
     <div className="form__campo-digitacao">
       <label htmlFor={tipo}>{label}</label>
-
-      <input type={tipo} placeholder={placeholder} required id={tipo} />
+<input
+  type={tipo}
+  placeholder={placeholder}
+  required
+  id={tipo}
+  value={value}
+  onChange={(evento) => {setValue(evento.target.value);//é um evento que fazemos uma manipulação do que tem dentro dele
+  }}
+/>
     </div>
   );
 }
@@ -16,10 +23,8 @@ CampoDeDigitacao.propTypes = {
   label: PropTypes.string.isRequired,
   tipo: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
+  value: PropTypes.string,
+  setValue: PropTypes.func.isRequired,
 };
 
-// Valores padrão para props opcionais
-CampoDeDigitacao.defaultProps = {
-  placeholder: "",
-};
 export default CampoDeDigitacao;
--- src/components/PaginaDeLogin.jsx
+++ src/components/PaginaDeLogin.jsx
@@ -1,7 +1,16 @@
 import "../styles/PaginaDeLogin.css";
 import PropTypes from "prop-types";
 
-function PaginaDeLogin({ children }) {
+function PaginaDeLogin({email,senha, children }) {
+    //sempre que manipular evento, é boa prática iniciar com handle
+  const handleSubmit = (event) => {
+    event.preventDefault(); //evita o comportamento padrão de atualizar a página
+    console.log('email  ' + email);
+    console.log('senha  ' + senha);
+
+  }
+
+  
   return (
     <div className="container-login  ">
       <img
@@ -9,7 +18,7 @@ function PaginaDeLogin({ children }) {
         alt="mulher negra de cabelos crespos, com oculos mechendo no computador e a logo da codeconnect"
       />
       <section>
-        <form>{children}</form>
+        <form onSubmit={handleSubmit}>{children}</form>
       </section>
     </div>
   );
@@ -18,6 +27,8 @@ function PaginaDeLogin({ children }) {
 // Definição dos tipos esperados para as props
 PaginaDeLogin.propTypes = {
   children: PropTypes.node.isRequired, // 'node' aceita qualquer conteúdo renderizável (elementos, strings, etc.)
+  email: PropTypes.node.isRequired,
+  senha: PropTypes.node.isREquired
 };
 
 export default PaginaDeLogin;
